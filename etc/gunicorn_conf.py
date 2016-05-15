126e35eac1c6b93dcb9c5ee8607b5e7505ae2e8a
David Miller <david@deadpansincerity.com>
2016-05-15 09:46:54 +0100

    Add fabfile and supervisord config

---
+++ etc/gunicorn_conf.py
@@ -0,0 +1,6 @@
+bind = "0.0.0.0:5567"
+logfile = "/tmp/obsidian.gunicorn.log"
+workers = 1
+timeout = 120
+accesslog = "/tmp/obsidian.access.log"
+errorlog = "/tmp/obsidian.error.log"
---
+++ fabfile.py
@@ -0,0 +1,42 @@
+from fabric.api import env, task, run, cd, prefix
+from fabric.operations import sudo
+
+env.hosts = ['192.168.2.27']
+env.user = 'pi'
+env.password = 'raspberry'
+
+@task
+def configure():
+    sudo('apt-get update')
+    sudo('apt-get install -y virtualenvwrapper python-dev libpq-dev libffi-dev')
+    run('mkdir -p /home/{0}/ohc'.format(env.user))
+    with cd('/home/{0}/ohc'.format(env.user)):
+        run('rm -rf outbreak')
+        run('git clone https://github.com/davidmiller/outbreak')
+    with cd('/home/{0}/ohc/outbreak'.format(env.user)):
+        with prefix('WORKON_HOME=$HOME/.virtualenvs'):
+            with prefix('source /etc/bash_completion.d/virtualenvwrapper'):
+                run('rmvirtualenv outbreak')
+                run('mkvirtualenv -a /home/{0}/ohc/outbreak outbreak'.format(env.user))
+
+        with prefix(". /usr/share/virtualenvwrapper/virtualenvwrapper.sh"):
+            with prefix("workon outbreak"):
+                run('pip install -r requirements.txt')
+                run("python manage.py migrate")
+                run("python manage.py collectstatic --noinput")
+                run('supervisord -c etc/pi.conf')
+
+    pass
+
+@task
+def deploy():
+    with cd('/home/{0}/ohc/outbreak'.format(env.user)):
+        run('git pull origin master')
+        with prefix(". /usr/share/virtualenvwrapper/virtualenvwrapper.sh"):
+            with prefix("workon outbreak"):
+                run('pip install -r requirements.txt')
+                run("python manage.py migrate")
+                run("python manage.py collectstatic --noinput")
+                run('supervisorctl -c etc/pi.conf restart gunicorn')
+
+    pass
